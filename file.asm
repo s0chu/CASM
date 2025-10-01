63bb6c40890cbc6e3da063004f4e1ba425e5c01e
s0chu <didididi328@gmail.com>
2025-10-01 17:59:48 +0300

    look around

--- file.asm
+++ file.asm
@@ -1,1 +1,24 @@
-sdafas
+                global _start
+                extern printf
+
+                section .text
+
+
+_start:
+                [section .data]
+format:     db "it works %d hello" , 10
+                [section .text]
+
+            mov rax , 100
+            mov rdi , format
+            mov rsi , [value]
+
+            call printf
+
+            mov rax , 60
+            mov rdi , 0
+            syscall 
+
+                section .data
+value:      db 0
+
