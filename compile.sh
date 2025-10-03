14ff5774826534dda8c5b672762f18cf8d9c3ba4
s0chu <didididi328@gmail.com>
2025-10-03 21:09:39 +0300

    extended compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,6 @@
-nasm -f elf64 main.asm -o main.o && nasm -f elf64 string.asm -o string.o && nasm -f elf64 avl.asm -o avl.o && ld main.o string.o avl.o -o main -lc -dynamic-linker /lib64/ld-linux-x86-64.so.2 && ./main 
+nasm -f elf64 main.asm -o main.o  
+nasm -f elf64 string.asm -o string.o
+nasm -f elf64 avl.asm -o avl.o 
+ld main.o string.o avl.o -o main -lc -dynamic-linker /lib64/ld-linux-x86-64.so.2 
+./main 
 rm main.o string.o avl.o main
