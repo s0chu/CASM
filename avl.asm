c160595c25ed91f4a8cdf72c4f3108b81b2b7820
s0chu <didididi328@gmail.com>
2025-10-03 19:32:32 +0300

    OOPified

---
+++ avl.asm
@@ -0,0 +1,167 @@
+                section .text 
+                
+                extern printf 
+                extern strcpy 
+                extern strcmp 
+                extern strlen 
+
+                extern format_decimal
+                extern format_node 
+                extern format_string 
+
+                global left 
+                global right
+                global height
+                global key
+                global value 
+
+                global print_node 
+                global new_node 
+
+%macro create_new_node 0
+                [section .bss]
+            %%start_adr: resq 2  ; left right child
+                         resd 1  ; max height
+                         resb 20 ; label 
+                         resq 1  ; value 
+
+                [section .text]
+            mov rax , %%start_adr
+%endmacro
+
+init_node: ; ptr , name , value
+    push rbp
+    mov rbp , rsp
+
+    mov qword [rdi + left] , 0
+    mov qword [rdi + right] , 0
+    mov dword [rdi + height] , 0
+    mov qword [rdi + value] , rdx
+    mov byte [rdi + key] , 0
+
+    add rdi , key
+    push rdi
+    mov rsi , rsi
+    call strcpy
+
+    mov rsp , rbp
+    pop rbp
+    ret
+
+new_node:
+    push rbp 
+    mov rbp , rsp
+    
+    create_new_node
+
+    push rax 
+
+    mov rdx , rsi
+    mov rsi , rdi 
+    mov rdi , rax 
+    call init_node 
+    
+    mov rax , [rbp - 8]
+
+    mov rsp , rbp 
+    pop rbp 
+    ret
+    
+print_node:
+    push rbp 
+    mov rbp , rsp
+
+    mov rax , rdi
+
+    mov rdi , format_node 
+    mov rsi , [rax + left]
+    mov rdx , [rax + right]
+    mov ecx , [rax + height]
+    mov r8 , rax 
+    add r8 , key
+    mov r9 , [rax + value]
+    xor rax , rax 
+
+    call printf
+
+    mov rsp , rbp
+    pop rbp
+    ret
+
+
+get_height: ; ptr -> int
+    push rbp 
+    mov rbp , rsp
+
+    cmp rdi , 0
+    jz .null
+        mov eax , dword [rdi + height]
+        jmp .fi
+    .null:
+        mov eax , 0
+    .fi:
+
+    mov rsp , rbp 
+    pop rbp 
+    ret
+
+recalculate: ; ptr -> void
+    push rbp 
+    mov rbp , rsp 
+
+    %define ptr -8
+    push rdi 
+
+    mov rdi , [rdi + left]
+    call get_height 
+
+    push rax 
+    %define hl -16
+
+    mov rdi , [rbp + ptr]
+    mov rdi , [rdi + right]
+    call get_height 
+
+    push rax 
+    %define hr -24
+
+    mov rdi , [rbp + ptr]
+    
+    mov rax , [rbp + hl]
+    cmp rax , [rbp + hr]
+
+    ja .done 
+    mov rax , [rbp + hr]
+
+    .done:
+
+    mov dword [rdi + height] , eax
+    mov rsp , rbp 
+    pop rbp 
+    ret
+
+left_rotate: ; ptr -> ptr
+    push rbp 
+    mov rbp , rsp 
+
+
+
+
+
+    mov rsp , rbp
+    pop rbp
+    ret
+
+
+
+right_rotate: ; ptr -> ptr
+
+
+
+                section .data
+            root: dq 0
+            left:   equ  0
+            right:  equ  8
+            height: equ 16
+            key:    equ 20
+            value:  equ 40
---
+++ string.asm
@@ -0,0 +1,121 @@
+                    
+                    section .text
+                    
+                    global format_decimal
+                    global format_node
+                    global format_string 
+
+                    global strcpy
+                    global strlen
+                    global strcmp
+
+strlen:
+    push rbp
+    mov rbp , rsp
+
+    sub rsp , 8
+
+    %define i -8
+    mov dword [rbp + i] , 0
+
+    .for:
+        mov eax , [rbp + i]
+        cmp byte [rdi + rax] , 0
+        jz .done
+        inc dword [rbp + i]
+        jmp .for
+    .done:
+
+    %undef i
+    mov rsp , rbp
+    pop rbp
+    ret
+
+strcpy: 
+    push rbp
+    mov rbp , rsp
+
+    sub rsp , 8
+    %define i -8
+
+    mov dword [rbp + i] , 0
+
+    .for:
+        mov eax , dword [rbp + i]
+        mov bl , [rsi + rax]
+        mov byte [rdi + rax] , bl
+        cmp byte [rsi + rax] , 0
+        jz .done
+        inc dword [rbp + i]
+        jmp .for
+    .done:
+
+    mov rsp , rbp
+    pop rbp
+
+    ; %undef i
+    ret
+
+
+
+strcmp:
+    push rbp
+    mov rbp , rsp
+
+    .for:
+        cmp byte [rdi] , 0
+        jnz .else
+        
+        cmp byte [rsi] , 0
+        jnz .else 
+
+            jmp .ret0
+
+        .else:
+
+            mov al , byte [rdi]
+            cmp al , byte [rsi]
+
+            jb .ret2
+            ja .ret1
+
+        inc rdi
+        inc rsi
+        jmp .for
+    .done:
+
+    .ret0:
+        mov rax , 0
+        jmp strcmp.finish
+    .ret1:
+        mov rax , 1
+        jmp strcmp.finish
+    .ret2:
+        mov rax , -1
+        jmp strcmp.finish
+    .finish:
+
+    mov rsp , rbp
+    pop rbp
+    ret
+
+                section .data 
+
+            format_decimal: db "%d" , 10 , 0
+            format_string:  db "%s" , 10 , 0
+            format_node:    db "left:%p right:%p height:%d key: %s value: %d" , 10
+    ; 1 0 -> 0
+    ; 1011 ->     10110
+    ; 1011        01010 -> -10
+
+                ; 101
+                ; 011
+
+                ; 101
+                ; 001
+
+                ; 111
+                ; 001
+
+                ; 111
+                ; 011
